cb023910f989823f1570b18a2e258661e60bd619
Björn Sjögren <lokanx@gmail.com>
2021-12-15 23:02:43 +0100

    updates

--- portainer-update.sh
+++ portainer-update.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-#docker pull cr.portainer.io/portainer/portainer-ce:latest
-#docker stop portainer
-#docker rm portainer
+docker pull cr.portainer.io/portainer/portainer-ce:latest
+docker stop portainer
+docker rm portainer
 docker run -d -p 9000:9000 --name portainer \
     --restart always \
     -v /var/run/docker.sock:/var/run/docker.sock  \
     -v portainer_data:/data  \
     cr.portainer.io/portainer/portainer-ce:latest
-
 
--- watchtower.sh
+++ watchtower.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-
+docker pull containrrr/watchtower
+docker stop watchtower
+docker rm watchtower
 docker run -d \
   --name watchtower \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -e WATCHTOWER_POLL_INTERVAL=43200 -e WATCHTOWER_LABEL_ENABLE=true -e WATCHTOWER_MONITOR_ONLY=true \
-  --env-file ./.watchtower.env --label=com.centurylinklabs.watchtower.enable=true \
+  -e WATCHTOWER_POLL_INTERVAL=20000 -e WATCHTOWER_LABEL_ENABLE=true -e WATCHTOWER_MONITOR_ONLY=true \
+  --env-file ./.watchtower.env \
   containrrr/watchtower --debug
