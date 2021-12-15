93aacfea2fed5c7204c959799f092112c94123a3
Björn Sjögren <lokanx@gmail.com>
2021-12-15 23:16:35 +0100

    Updates

--- watchtower.sh
+++ watchtower.sh
@@ -4,6 +4,7 @@ docker stop watchtower
 docker rm watchtower
 docker run -d \
   --name watchtower \
+  --restart always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -e WATCHTOWER_POLL_INTERVAL=20000 -e WATCHTOWER_LABEL_ENABLE=true -e WATCHTOWER_MONITOR_ONLY=true \
   --env-file ./.watchtower.env \
