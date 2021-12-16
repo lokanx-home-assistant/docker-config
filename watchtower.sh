8785158de08266d3e63a400f61e6e3c3416bde5c
Björn Sjögren <lokanx@gmail.com>
2021-12-16 23:50:05 +0100

    Updates

--- watchtower.sh
+++ watchtower.sh
@@ -11,6 +11,7 @@ docker run -d \
   -e WATCHTOWER_LABEL_ENABLE=true  \
   -e WATCHTOWER_MONITOR_ONLY=true \
   -e WATCHTOWER_NOTIFICATIONS_LEVEL=info \
+  -e WATCHTOWER_NO_STARTUP_MESSAGE=true \
   -e WATCHTOWER_NOTIFICATIONS=shoutrrr \
   -e WATCHTOWER_NOTIFICATION_TEMPLATE='{{range .}}{{println}}{ {{println}}"date":"{{.Time.Format "2006-01-02 15:04:05"}}",{{println}}"level":"{{.Level}}",{{println}}"message":"{{.Message}}"{{println}} } {{println}}{{end}}' \
   containrrr/watchtower --debug
