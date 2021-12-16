1ae778ca739a8422e1aa713ed2d23c230a6ed639
Björn Sjögren <lokanx@gmail.com>
2021-12-16 01:07:13 +0100

    Fixes

--- watchtower.sh
+++ watchtower.sh
@@ -5,7 +5,12 @@ docker rm watchtower
 docker run -d \
   --name watchtower \
   --restart always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -e WATCHTOWER_POLL_INTERVAL=20000 -e WATCHTOWER_LABEL_ENABLE=true -e WATCHTOWER_MONITOR_ONLY=true \
   --env-file ./.watchtower.env \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -e WATCHTOWER_POLL_INTERVAL=20000  \
+  -e WATCHTOWER_LABEL_ENABLE=true  \
+  -e WATCHTOWER_MONITOR_ONLY=true \
+  -e WATCHTOWER_NOTIFICATIONS_LEVEL=info \
+  -e WATCHTOWER_NOTIFICATIONS=shoutrrr \
+  -e WATCHTOWER_NOTIFICATION_TEMPLATE='{{range .}}{{println}}{ {{println}}"date":"{{.Time.Format "2006-01-02 15:04:05"}}",{{println}}"level":"{{.Level}}",{{println}}"message":"{{.Message}}"{{println}} } {{println}}{{end}}' \
   containrrr/watchtower --debug
